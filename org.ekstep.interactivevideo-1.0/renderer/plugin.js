b6169a7dbc0dc743c0a52653995b8e95b256b5b1
Gourav More <gourav_m@tekditechnologies.com>
2017-04-02 14:05:43 +0530

    fix: get src from config

--- org.ekstep.interactivevideo-1.0/renderer/plugin.js
+++ org.ekstep.interactivevideo-1.0/renderer/plugin.js
@@ -3,12 +3,11 @@ Plugin.extend({
     _isContainer: false,
     _render: true,
     initPlugin: function(data) {
-    	console.log(data);
 		var parentDiv = document.getElementById(Renderer.divIds.gameArea);
         var div = document.getElementById("testDiv");
         div = document.createElement('div');
         div.style.width = "100%";
-        div.innerHTML = '<div id="container" align="center"><video style="position:relative; z-index:-1;" width="640" height="320" autoplay><source src="http://media.w3.org/2010/05/sintel/trailer.mp4" type="video/mp4"/></video><div id="custom-message" style="position:relative; color: rgb(230, 200, 98);z-index:1;top: -200px; ">CENTERED TEXT ABOVE</div></div>';
+        div.innerHTML = '<div id="container" align="center"><video style="position:relative; z-index:-1; width="650" height="325" autoplay><source src="'+data.video+'" type="video/mp4"/></video><div id="custom-message" style="position:relative; color: rgb(230, 200, 98);z-index:1;top: -200px; ">CENTERED TEXT ABOVE</div></div>';
         parentDiv.insertBefore(div, parentDiv.childNodes[0]);
         this._self = new createjs.DOMElement(div);
 
