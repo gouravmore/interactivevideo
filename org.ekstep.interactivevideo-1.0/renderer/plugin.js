4f5e8e84b3640c3fcd0ac3dd3e9f2e95b1939245
Ashwin Date <coolbung@gmail.com>
2017-04-02 17:02:55 +0530

    feat:added error handling to questions array

--- org.ekstep.interactivevideo-1.0/renderer/plugin.js
+++ org.ekstep.interactivevideo-1.0/renderer/plugin.js
@@ -3,11 +3,31 @@ Plugin.extend({
     _isContainer: false,
     _render: true,
     initPlugin: function(data) {
+    	if (typeof(data.video) != 'string') {
+    		console.log('Video not defined');
+    		console.log(data.video);
+    		return false;
+    	}
+
+    	if (typeof(data.questions) != 'object') {
+    		console.log('Questions not defined');
+    		console.log(data.questions);
+    		return false;
+    	}
+
 		var parentDiv = document.getElementById(Renderer.divIds.gameArea);
         var div = document.getElementById("testDiv");
         div = document.createElement('div');
         div.style.width = "100%";
-        div.innerHTML = '<div id="container" align="center"><video style="position:relative; z-index:-1; width="650" height="325" autoplay><source src="'+data.video+'" type="video/mp4"/></video><div id="custom-message" style="position:relative; color: rgb(230, 200, 98);z-index:1;top: -200px; ">CENTERED TEXT ABOVE</div></div>';
+        var videohtml = [];
+        videohtml.push('<div id="container" align="center">');
+        videohtml.push('<video style="position:relative; z-index:-1;" width="650" height="325" autoplay>');
+        videohtml.push('<source src="'+data.video+'" type="video/mp4"/>');
+        videohtml.push('</video>');
+        videohtml.push('<div id="interactivevideo-question-container" style="display:none; position:relative; color: rgb(230, 200, 98);z-index:1;top: -330px; background: #000; opacity: 0.8; height: 330px; "></div>');
+        videohtml.push('</div>');
+        div.innerHTML = videohtml.join('');
+
         parentDiv.insertBefore(div, parentDiv.childNodes[0]);
         this._self = new createjs.DOMElement(div);
 
@@ -29,32 +49,38 @@ Plugin.extend({
 			var video = $(this).get(0);
 			var iNow = video.currentTime;
 			jQuery.each(timer, function(key, value) {
+				if (typeof(value.data.question) != 'string') {
+					console.log("Invalid question identifier : " + identifier);
+					return false;
+				}
+
 				if(a != Math.round(iNow))
 				{
 					flag =false;
 				}
+
 				if (Math.round(iNow) == value.sec && !flag) {
-					jQuery("#custom-message").show();
+					jQuery("#interactivevideo-question-container").show();
 
 			    	var html = [];
-			    	html.push('<div class="ui-form" id="interactivevideo-question-container"><div class="grouped-fields">');
-			    	html.push('<h2>' + value.data.name + '</h2>');
+			    	html.push('<div class="ui-form"><div class="grouped-fields">');
+			    	html.push('<p>' + value.data.question + '</p>');
 			    	html.push('<ul>');
 			    	jQuery.each(value.data.options, function(key, val) {
 			    		html.push('<li><input type="radio" id="option'+val.value.resindex+'" name="interactivevideo-question" value="'+val.value.resvalue+'"><label for="option'+val.value.resindex+'">'+val.value.text+'</label><div class="check"></div></li>');
 			    	});
 			    	html.push('</ul>');
-			    	html.push('<p><button id="videocover" class="button">Submit</button></p>');
+			    	html.push('<p><button id="videocover">Submit</button></p>');
 			    	html.push('</div></div>');
 
-					jQuery('#custom-message').html(html.join(''));
+					jQuery('#interactivevideo-question-container').html(html.join(''));
 
 					video.pause();
 					var elem = document.getElementById('videocover');
 					elem.addEventListener('click', function(event) {
 						var video = $("video").get(0);
 						video.play();
-						jQuery("#custom-message").hide();
+						jQuery("#interactivevideo-question-container").hide();
 
 						flag = true;
 						a = Math.round(iNow);
@@ -69,7 +95,7 @@ Plugin.extend({
     	console.log(question);
     	var html = [];
     	html.push('<div id="interactivevideo-question-container">');
-    	html.push('<p>' + question.data.name + '</p>');
+    	html.push('<h2>' + question.data.questions + '</p>');
     	html.push('<ul>');
     	jQuery.each(question.data.options, function(key, val) {
     		html.push('<li><label><input type="radio" name="question" value="'+val.value.resvalue+'" />' + val.value.resvalue + '</label></li>');
